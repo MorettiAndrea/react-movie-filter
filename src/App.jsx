c5a8221b1d71fbed2b3c821ca4ab5a184a6e1140
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-05-28 18:12:47 +0200

    fine manca bonus e stile

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import movies from "./data/movies";
+
+export default function App() {
+  const [selectedGenre, setSelectedGenre] = useState("Seleziona un genere");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  useEffect(() => {
+    if (selectedGenre === "Seleziona un genere") {
+      setFilteredMovies(movies);
+    } else {
+      const filtered = movies.filter((movie) => movie.genre === selectedGenre);
+      setFilteredMovies(filtered);
+    }
+  }, [selectedGenre]);
+
+  const handleGenreChange = (event) => {
+    setSelectedGenre(event.target.value);
+  };
+
+  return (
+    <>
+      <label htmlFor="categoria">Scegli una categoria:</label>
+      <select onChange={handleGenreChange} value={selectedGenre}>
+        <option value="Seleziona un genere">Seleziona un genere</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+      </select>
+
+      <h2>Lista Film:</h2>
+      <ul>
+        {filteredMovies.map((movie, index) => (
+          <li key={index}>
+            {movie.title} ({movie.genre})
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
